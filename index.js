179bb995e77181db7274433e94158c351649e661
Deepak Suthar <deepaksuthar40128@gmail.com>
2024-10-02 11:27:20 +0530

    api limit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const axios = require("axios");
 const jsdom = require("jsdom");
-const express = require("express");
+const express = require("express"); 
 const cors = require("cors");
-const { JSDOM } = jsdom; 
+const { JSDOM } = jsdom;
 
 const app = express();
 
@@ -19,54 +19,63 @@ app.get("/rating/:handle", (req, res) => {
 });
 
 app.get("/handle/:handle", async (req, res) => {
-  try { 
-    if(req.params.handle==='favicon.ico')res.send({ success: false, error: err }); 
-    const resdata = await fetch(`https://www.codechef.com/users/${req.params.handle}`);
-    let d = await resdata.text();
-    let data = { data: d };
-    let heatMapDataCursour1 = 
-      data.data.search("var userDailySubmissionsStats =") +
-      "var userDailySubmissionsStats =".length;
-    let heatMapDataCursour2 = data.data.search("'#js-heatmap") - 9;
-    let heatDataString = data.data.substring(
-      heatMapDataCursour1,
-      heatMapDataCursour2
-    );  
-    let headMapData = JSON.parse(heatDataString);
-    let allRating =
-      data.data.search("var all_rating = ") + "var all_rating = ".length;
-    let allRating2 = data.data.search("var current_user_rating =") - 6;
-    let ratingData = JSON.parse(data.data.substring(allRating, allRating2));
-    let dom = new JSDOM(data.data);
-    let document = dom.window.document;
-    res.status(200).send({
-      success: true,
-      profile: document.querySelector(".user-details-container").children[0]
-        .children[0].src,
-      name: document.querySelector(".user-details-container").children[0]
-        .children[1].textContent,
-      currentRating: parseInt(
-        document.querySelector(".rating-number").textContent
-      ),
-      highestRating: parseInt(
-        document
-          .querySelector(".rating-number")
-          .parentNode.children[4].textContent.split("Rating")[1]
-      ),
-      countryFlag: document.querySelector(".user-country-flag").src,
-      countryName: document.querySelector(".user-country-name").textContent,
-      globalRank: parseInt(
-        document.querySelector(".rating-ranks").children[0].children[0]
-          .children[0].children[0].innerHTML
-      ),
-      countryRank: parseInt(
-        document.querySelector(".rating-ranks").children[0].children[1]
-          .children[0].children[0].innerHTML
-      ),
-      stars: document.querySelector(".rating").textContent || "unrated",
-      heatMap: headMapData,
-      ratingData,
-    });
+  try {
+    if (req.params.handle === "favicon.ico")
+      res.send({ success: false, error: err });
+    const resdata = await fetch(
+      `https://www.codechef.com/users/${req.params.handle}`
+    ); 
+    if (resdata.status == 200) {
+      let d = await resdata.text(); 
+      let data = { data: d };
+      let heatMapDataCursour1 =
+        data.data.search("var userDailySubmissionsStats =") +
+        "var userDailySubmissionsStats =".length;
+      let heatMapDataCursour2 = data.data.search("'#js-heatmap") - 9;
+      let heatDataString = data.data.substring(
+        heatMapDataCursour1,
+        heatMapDataCursour2
+      );
+      let headMapData = JSON.parse(heatDataString);
+      let allRating =
+        data.data.search("var all_rating = ") + "var all_rating = ".length;
+      let allRating2 = data.data.search("var current_user_rating =") - 6;
+      let ratingData = JSON.parse(data.data.substring(allRating, allRating2));
+      let dom = new JSDOM(data.data);
+      let document = dom.window.document;
+      res.status(200).send({
+        success: true,
+        profile: document.querySelector(".user-details-container").children[0]
+          .children[0].src,
+        name: document.querySelector(".user-details-container").children[0]
+          .children[1].textContent,
+        currentRating: parseInt(
+          document.querySelector(".rating-number").textContent
+        ),
+        highestRating: parseInt(
+          document
+            .querySelector(".rating-number")
+            .parentNode.children[4].textContent.split("Rating")[1]
+        ),
+        countryFlag: document.querySelector(".user-country-flag").src,
+        countryName: document.querySelector(".user-country-name").textContent,
+        globalRank: parseInt(
+          document.querySelector(".rating-ranks").children[0].children[0]
+            .children[0].children[0].innerHTML
+        ),
+        countryRank: parseInt(
+          document.querySelector(".rating-ranks").children[0].children[1]
+            .children[0].children[0].innerHTML
+        ),
+        stars: document.querySelector(".rating").textContent || "unrated",
+        heatMap: headMapData,
+        ratingData,
+      });
+    }
+    else{
+      for(let i=0;i<100000;i++){}
+      res.redirect(`/handle/${req.params.handle}`);
+    }
   } catch (err) { 
     res.send({ success: false, error: err });
   }
@@ -77,13 +86,16 @@ app.get("/", (req, res) => {
   // res.status(200).send("Hi you are at right endpoint just add /handle_of_user at the end of url Github-repo(https://github.com/deepaksuthar40128/Codechef-API) Thanks for 🌟");
 });
 
-
 app.get("/:handle", (req, res) => {
-  const handle = req.params.handle
-  if(handle&&handle.length&&!handle.includes('.'))res.redirect(`/handle/${handle}`);
+  const handle = req.params.handle;
+  if (handle && handle.length && !handle.includes("."))
+    res.redirect(`/handle/${handle}`);
   else
-  res.send({ success: false, error: `Invalid Endpoint kindly change your url from https://codechef-api.vercel.app/${handle} to https://codechef-api.vercel/handle/${handle} or may be incorrect handle` });
-})
+    res.send({
+      success: false,
+      error: `Invalid Endpoint kindly change your url from https://codechef-api.vercel.app/${handle} to https://codechef-api.vercel/handle/${handle} or may be incorrect handle`,
+    });
+});
 
 const PORT = process.env.PORT || 8800;
 app.listen(PORT, () => {
